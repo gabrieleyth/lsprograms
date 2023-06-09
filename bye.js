221027e7f6c09eb9ff36e10b86d72463405b1daf
gabrieleyth <gabriel.leyth@outlook.com>
2023-06-09 08:34:08 +0000

    bye program

---
+++ bye.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync');
+
+console.log('Welcome to Calculator!');
+
+console.log("What's the first number?");
+let number1 = readline.question();
+
+console.log("What's the second number?");
+let number2 = readline.question();
+
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = parseInt(readline.question());
+
+let output;
+if (operation === 1) {
+  output = Number(number1) + Number(number2);
+} else if (operation === 2) {
+  output = Number(number1) - Number(number2);
+} else if (operation === 3) {
+  output = Number(number1) * Number(number2);
+} else if (operation === 4) {
+  output = Number(number1) / Number(number2);
+}
+
+console.log(`the result is ${output}.`);
