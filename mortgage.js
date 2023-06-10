91f4192546c05d51e089845f128610a001e495d7
gabrieleyth <gabriel.leyth@outlook.com>
2023-06-10 11:32:48 +0000

    cool mortgage calculator

--- mortgage.js
+++ mortgage.js
@@ -1,19 +1,68 @@
 const readline = require('readline-sync');
 
-console.log('What is the loan duration in months?');
-let loanDurationMonths = readline.question();
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
-console.log('What is the loan amount?');
-let loanAmount = readline.question();
+function invalidWord(word) {
+  return word !== 'y' && word !== 'n';
+}
 
-console.log('what is the APR?');
-let annualIntRate = readline.question();
 
-let monthlyIntRate = (annualIntRate / 12) / 100;
+function displayingMortgage() {
+  console.log('What is the loan duration in months?');
+  let loanDurationMonths = readline.question();
 
-let monthlyPayment = loanAmount * (monthlyIntRate /
-(1 - Math.pow((1 + monthlyIntRate), (-loanDurationMonths))));
+  while (invalidNumber(loanDurationMonths)) {
+    console.log('Invalid number, try again.');
+    loanDurationMonths = readline.question();
+  }
 
-let refinedMonthly = monthlyPayment.toFixed(2);
+  console.log('What is the loan amount?');
+  let loanAmount = readline.question();
 
-console.log(`Your monthly payment shall be $${refinedMonthly}.`);
+  while (invalidNumber(loanAmount)) {
+    console.log('Invalid number, try again.');
+    loanAmount = readline.question();
+  }
+
+  console.log('what is the APR?');
+  let annualIntRate = readline.question();
+
+  while (invalidNumber(annualIntRate)) {
+    console.log('Invalid number, try again.');
+    annualIntRate = readline.question();
+  }
+
+  let monthlyIntRate = (annualIntRate / 12) / 100;
+
+  let mathPow = Math.pow((1 + monthlyIntRate), (-loanDurationMonths));
+
+  let monthlyPayment = loanAmount * (monthlyIntRate / (1 - mathPow));
+
+  let refinedMonthly = monthlyPayment.toFixed(2);
+
+  console.log(`Your monthly payment shall be $${refinedMonthly}.`);
+}
+
+displayingMortgage();
+
+function anotherCalculation() {
+  console.log('Feeling for another calculation? y/n ');
+  let answer = readline.question();
+
+  while (invalidWord(answer)) {
+    console.log('Invalid input, try again.');
+    answer = readline.question();
+  }
+
+  if (answer === 'y') {
+    displayingMortgage();
+  } else {
+    console.log('See you later alligator!');
+  }
+}
+
+anotherCalculation();
+
+
